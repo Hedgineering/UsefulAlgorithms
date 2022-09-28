97ad72079f5dd5a44df2bc1b0ce462bcdb0a6fcb
Rahul Hegde <rhegde.hrahul@gmail.com>
2022-09-28 11:44:45 -0400

    initial commit

---
+++ DFS_WDG.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace Hedgineering.Algorithms.WeightedDFS;
+
+/**
+ * This is a DFS for a weighted directed graph
+ * 
+ * Implementation: 
+ *   Node of some type T, 
+ *   Edges with startNode, endNode, and weight
+ *   Graph represented with an adjacency matrix such that matrix[startNode][endNode] = weight
+ */
+public class Node<T> {
+    public T value;
+}
+
+public class Edge<T, U> where U : IComparable<U> {
+    Node<T> start;
+    Node<T> end;
+    U weight;
+}
